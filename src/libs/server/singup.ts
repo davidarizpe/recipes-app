03bc8f886d28acbce5ae4f15abb95b5f2e991129
David Arzipe <daviaaljaque@gmail.com>
2024-12-19 13:57:32 -0600

    sing up

---
+++ src/libs/server/singup.ts
@@ -0,0 +1,46 @@
+"use server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createUser = async (values: {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+}) => {
+  try {
+    const equalEmail = await prisma.users.findFirst({
+      where: {
+        email: values.email,
+      },
+    });
+
+    const equalUsername = await prisma.users.findFirst({
+      where: {
+        username: values.username,
+      },
+    });
+
+    if (equalEmail || equalUsername) {
+      return { success: false, message: "Username or email already exists" };
+    }
+
+    await prisma.users.create({
+      data: {
+        name: `${values.firstname} ${values.lastname}`,
+        username: values.username || `${values.firstname}${values.lastname}`,
+        email: values.email,
+        password: values.password,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: "Error creating user" };
+  }
+
+  return { success: true, message: "User created successfully" };
+};
+
+export default createUser;
